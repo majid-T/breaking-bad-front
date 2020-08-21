c1f5a4e3b1c496a3dec688589a6ca76be76d4637
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-08-22 01:43:30 +0430

    landing page css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Navigation from "./components/Navigation";
 import Charachters from "./components/Charachters";
 import Episodes from "./components/Episodes";
+import Landing from "./components/Landing";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
         <Navigation />
 
         <Switch>
+          <Route exact path="/" component={Landing} />
+
           <Route
             path="/charachters"
             render={(props) => <Charachters {...props} baseUrl={baseUrl} />}
